ce10f52dfb1477801195fcc2534eabfecc8f03fc
Daniel Ernany <ddernany@gmail.com>
2022-08-02 10:40:04 -0300

    feat: tela de home responsiva para mobile e desktop

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:facebook_interface/componentes/botao_circulo.dart';
 import 'package:facebook_interface/componentes/cartao_postagem.dart';
 import 'package:facebook_interface/dados/models/postagem.dart';
 import 'package:facebook_interface/uteis/paleta_cores.dart';
+import 'package:facebook_interface/uteis/responsivo.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 
@@ -14,60 +15,133 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            centerTitle: false,
-            floating: true,
-            title: const Text(
-              'facebook',
-              style: TextStyle(
-                color: PaletaCores.AZUL_FACEBOOK,
-                fontWeight: FontWeight.bold,
-                fontSize: 28,
-                letterSpacing: -1.2,
-              ),
-            ),
-            actions: [
-              BotaoCirculo(
-                icone: Icons.search,
-                iconeTamanho: 30,
-                onPressed: () {},
-              ),
-              BotaoCirculo(
-                icone: LineIcons.facebookMessenger,
-                iconeTamanho: 30,
-                onPressed: () {},
-              ),
-            ],
-          ),
-          SliverToBoxAdapter(
-            child: AreaCriarPostagem(usuario: usuarioAtual),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
-            sliver: SliverToBoxAdapter(
-              child: AreaEstoria(
-                usuario: usuarioAtual,
-                estorias: estorias,
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                Postagem postagem = postagens[index];
-                return CartaoPostagem(
-                  postagem: postagem,
-                );
-              },
-              childCount: postagens.length,
-            ),
-          ),
-        ],
+    return const Scaffold(
+      body: Responsivo(
+        mobile: HomeMobile(),
+        desktop: HomeDesktop(),
       ),
+    );
+  }
+}
+
+class HomeMobile extends StatelessWidget {
+  const HomeMobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          floating: true,
+          title: const Text(
+            'facebook',
+            style: TextStyle(
+              color: PaletaCores.AZUL_FACEBOOK,
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              letterSpacing: -1.2,
+            ),
+          ),
+          actions: [
+            BotaoCirculo(
+              icone: Icons.search,
+              iconeTamanho: 30,
+              onPressed: () {},
+            ),
+            BotaoCirculo(
+              icone: LineIcons.facebookMessenger,
+              iconeTamanho: 30,
+              onPressed: () {},
+            ),
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: AreaCriarPostagem(usuario: usuarioAtual),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: AreaEstoria(
+              usuario: usuarioAtual,
+              estorias: estorias,
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              Postagem postagem = postagens[index];
+              return CartaoPostagem(
+                postagem: postagem,
+              );
+            },
+            childCount: postagens.length,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeDesktop extends StatelessWidget {
+  const HomeDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          floating: true,
+          title: const Text(
+            'facebook',
+            style: TextStyle(
+              color: PaletaCores.AZUL_FACEBOOK,
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              letterSpacing: -1.2,
+            ),
+          ),
+          actions: [
+            BotaoCirculo(
+              icone: Icons.search,
+              iconeTamanho: 30,
+              onPressed: () {},
+            ),
+            BotaoCirculo(
+              icone: LineIcons.facebookMessenger,
+              iconeTamanho: 30,
+              onPressed: () {},
+            ),
+          ],
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: AreaEstoria(
+              usuario: usuarioAtual,
+              estorias: estorias,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: AreaCriarPostagem(usuario: usuarioAtual),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              Postagem postagem = postagens[index];
+              return CartaoPostagem(
+                postagem: postagem,
+              );
+            },
+            childCount: postagens.length,
+          ),
+        ),
+      ],
     );
   }
 }
